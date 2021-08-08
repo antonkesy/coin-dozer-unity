ecf1aab4447e1e4f989d46baf21626367335ef58
Anton Kesy <anton@kesy.de>
2021-08-08 16:08:13 +0200

    Simplify adding start coins

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -36,7 +36,7 @@ public class CoinManager : MonoBehaviour
             {
                 if (hit.point.x >= xMin && hit.point.x <= xMax && hit.point.y >= yMin && hit.point.y <= yMax)
                 {
-                    UserAddCoin(hit.point);
+                    AddCoin(hit.point);
                 }
             }
         }
@@ -64,54 +64,58 @@ public class CoinManager : MonoBehaviour
         return newCoin;
     }
 
-    private void UserAddCoin(Vector3 position)
+    //TODO refactor to add coin to always call this instead of raw? performance lost
+    private GameObject AddCoin(Vector3 position)
     {
         position.y = coinSpawnHeight;
         //O(n)
         //TODO add flag when full
-        foreach (var t in _coinPool)
+        //TODO linked list for free spots
+        foreach (var coin in _coinPool)
         {
-            if (!t.activeSelf)
+            if (!coin.activeSelf)
             {
-                t.transform.localPosition = position;
-                t.SetActive(true);
-                return;
+                coin.transform.localPosition = position;
+                coin.SetActive(true);
+                return coin;
             }
         }
 
         //else
-        _coinPool.Add(SpawnCoin(position));
+        var newCoin = SpawnCoin(position);
+        _coinPool.Add(newCoin);
+        return newCoin;
     }
 
     private void SetCoinsNewStartPos()
     {
         var newStartRows = 6;
         var newStartColumns = 6;
+
         var smallRowCount = 4;
         var spacer = .1f;
         var coinRadius = coinPrefab.transform.localScale.x;
-        var zPosCoin = 3.5F; //y start pos
+        var zPosCoin = 3.5F; //z start pos
         var xPosCoin = coinRadius * smallRowCount / 2 - coinRadius / 2 + spacer * smallRowCount / 2;
+        var spacePerCoin = coinRadius + spacer;
         //small row
-        for (var i = 0; i < smallRowCount; ++i)
-        {
-            _coinPool[i].transform.position = new Vector3(xPosCoin, 0, zPosCoin);
-            _coinPool[i].SetActive(true);
-            xPosCoin -= coinRadius + spacer;
-        }
-
+        SetRowCoins(xPosCoin, zPosCoin, spacePerCoin, smallRowCount);
         //full rows
         for (var rows = 0; rows < newStartRows; ++rows)
         {
             xPosCoin = coinRadius * newStartColumns / 2 - coinRadius / 2 + spacer * newStartColumns / 2;
             zPosCoin += coinRadius + spacer;
-            for (var column = 0; column < newStartColumns; ++column)
-            {
-                var index = newStartColumns * rows + column + smallRowCount;
-                _coinPool[index].transform.position = new Vector3(xPosCoin, 0, zPosCoin);
-                _coinPool[index].SetActive(true);
-                xPosCoin -= coinRadius + spacer;
-            }
+            SetRowCoins(xPosCoin, zPosCoin, spacePerCoin, newStartColumns);
+        }
+    }
+
+    private void SetRowCoins(float xPosStart, float zPos, float spacePerCoin, int amount)
+    {
+        for (var column = 0; column < amount; ++column)
+        {
+            var coin = AddCoin(new Vector3(xPosStart, 0, zPos));
+            coin.SetActive(true);
+            xPosStart -= spacePerCoin;
         }
     }
 }
